6bc26257a9822f11e9c1e6951691c91be5c7eb25
Tristan <tristandavisbrody@gmail.com>
2020-09-06 10:36:57 -0700

    second assignment updates

--- HW_1/mainscript.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-const incompleteHWLinks = document.querySelectorAll('.coming-soon');
-for(const a of incompleteHWLinks) {
-a.addEventListener('mouseover', function() {
-    a.innerText = 'Coming soon!';
-})
-
-}
-})
